ac6bb24e6c57a4dfd4e7f861dc819bf559313a12
S1ealth <S1ealth@me.com>
2018-10-30 16:50:57 +0300

    codefix

--- main.js
+++ main.js
@@ -11,27 +11,27 @@ let array = [4, -5, 6, 8, -20, 1, 0, 7, 15, 19];
 //   return maxNumberIndex
 // }
 // maxValue(array);
-function sort2(array) {
-  // unsorted array controller
-  for (let lastIndex = array.length - 1; lastIndex > 0; lastIndex--) {
-    // find max number and it's index in iteration
-    let maxNumberIndex = 0;
-    // console.log(maxNumber)
-    for (let i = 0; i <= lastIndex; i++) {
-      if (array[i] > array[maxNumberIndex]) {
-        maxNumberIndex = i;
-      }
-    }
-    // swap positions
-    // array[lastIndex];// a
-    // array[maxNumberIndex]; //b
-    let c = array[maxNumberIndex]; //save a to c
-    array[maxNumberIndex] = array[lastIndex] // swap
-    array[lastIndex] = c // restore
+// function sort2(array) {
+//   // unsorted array controller
+//   for (let lastIndex = array.length - 1; lastIndex > 0; lastIndex--) {
+//     // find max number and it's index in iteration
+//     let maxNumberIndex = 0;
+//     // console.log(maxNumber)
+//     for (let i = 0; i <= lastIndex; i++) {
+//       if (array[i] > array[maxNumberIndex]) {
+//         maxNumberIndex = i;
+//       }
+//     }
+//     // swap positions
+//     // array[lastIndex];// a
+//     // array[maxNumberIndex]; //b
+//     let c = array[maxNumberIndex]; //save a to c
+//     array[maxNumberIndex] = array[lastIndex] // swap
+//     array[lastIndex] = c // restore
 
-  }
-  return array;
-}
+//   }
+//   return array;
+// }
 // sort2(array);
 function bubleSort(array) {
   for (let i = 0; i < array.length - 1; i++) {
