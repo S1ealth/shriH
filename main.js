cc927bbf0eb3019de97e646290a858b7e3d1d877
S1ealth <S1ealth@me.com>
2018-10-30 14:23:14 +0300

    sorting by array modifier

--- main.js
+++ main.js
@@ -12,10 +12,29 @@ function maxValue(array) {
   return maxNumberIndex
 }
 maxValue(array);
-// function bubleSort(array) {
-
-//   return array
-// }
+function sort2(array) {
+  let lastIndexPosition = array.length-1 // b
+  console.log(lastIndexPosition);
+  let maxNumberIndex = 0;
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i])
+    if(array[i] > array[maxNumberIndex]) {
+      maxNumberIndex = i; // a
+      let a = array[maxNumberIndex];
+      let b = array[lastIndexPosition];
+      let c;
+      c = a;
+      a = b;
+      b = c;
+      array[i] = a;
+      array[lastIndexPosition] = b;
+      lastIndexPosition = lastIndexPosition-1;
+      console.log(array);
+    }
+  }
+  return array;
+}
+sort2(array);
 // bubleSort(array);
 
 // let a = 5;
